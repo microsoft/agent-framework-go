d1ee05fe2da77702d7a712849a6f12b5141e0499
qmuntal <quimmuntal@gmail.com>
2025-11-06 17:45:08 +0100

    add event and request types

---
+++ go/workflow/request.go
@@ -0,0 +1,55 @@
+// Copyright (c) Microsoft. All rights reserved.
+
+package workflow
+
+import (
+	"fmt"
+	"reflect"
+
+	"github.com/google/uuid"
+)
+
+// RequestPort contains information about an input port, including its input and output types.
+type RequestPort struct {
+	ID           string
+	RequestType  reflect.Type
+	ResponseType reflect.Type
+}
+
+// ExternalResponse represents a response to an external request.
+type ExternalResponse struct {
+	ID          string
+	RequestPort RequestPort
+	Data        Value
+}
+
+type ExternalRequest struct {
+	ID          string
+	RequestPort RequestPort
+	Data        Value
+}
+
+func NewExternalRequest(port RequestPort, id string, data any) *ExternalRequest {
+	if id == "" {
+		id = uuid.New().String()
+	}
+	if typ := reflect.TypeOf(data); typ != port.RequestType {
+		panic(fmt.Errorf("invalid request data type: expected %v, got %v", port.RequestType, typ))
+	}
+	return &ExternalRequest{
+		ID:          id,
+		RequestPort: port,
+		Data:        AnyValue(data),
+	}
+}
+
+func (r *ExternalRequest) NewResponse(data any) (ExternalResponse, error) {
+	if typ := reflect.TypeOf(data); typ != r.RequestPort.ResponseType {
+		return ExternalResponse{}, fmt.Errorf("invalid response type: expected %v, got %v", r.RequestPort.ResponseType, typ)
+	}
+	return ExternalResponse{
+		ID:          r.ID,
+		RequestPort: r.RequestPort,
+		Data:        AnyValue(data),
+	}, nil
+}
