9ca5a3b5f6972fdd1e721cbd3efba8f832b602a7
qmuntal <quimmuntal@gmail.com>
2025-11-25 10:36:03 +0100

    add more hosted tools

--- go/tool/hostedtool/hostedtool.go
+++ go/tool/hostedtool/hostedtool.go
@@ -1,17 +1,21 @@
 // Copyright (c) Microsoft. All rights reserved.
 
+// Package hostedtool provides definitions for hosted tools.
+//
+// Hosted tools do not themselves implement functionality. They are markers that can
+// be used to inform a service that the service is allowed to perform certain actions
+// if the service is capable of doing so.
 package hostedtool
 
-import "github.com/microsoft/agent-framework/go/tool"
+import (
+	"github.com/microsoft/agent-framework/go/message"
+	"github.com/microsoft/agent-framework/go/tool"
+)
 
 var _ tool.Tool = (*WebSearch)(nil)
 
 // WebSearch represents a hosted tool that can be specified to an
 // AI service to enable it to perform web searches.
-//
-// This tool does not itself implement web searches. It is a marker that can
-// be used to inform a service that the service is allowed to perform web
-// searches if the service is capable of doing so.
 type WebSearch struct {
 	AdditionalProperties map[string]any
 }
@@ -24,6 +28,39 @@ func (t *WebSearch) Description() string {
 	return ""
 }
 
+var _ tool.Tool = (*FileSearch)(nil)
+
+// FileSearch represents a hosted tool that can be specified to an AI service
+// to enable it to perform file search operations.
 type FileSearch struct {
 	AdditionalProperties map[string]any
+
+	Inputs             []message.Content
+	MaximumResultCount int
+}
+
+func (t *FileSearch) Name() string {
+	return "file_search"
+}
+
+func (t *FileSearch) Description() string {
+	return ""
+}
+
+var _ tool.Tool = (*CodeInterpreter)(nil)
+
+// CodeInterpreter represents a hosted tool that can be specified to an AI service
+// to enable it to execute code it generates.
+type CodeInterpreter struct {
+	AdditionalProperties map[string]any
+
+	Inputs []message.Content
+}
+
+func (t *CodeInterpreter) Name() string {
+	return "code_interpreter"
+}
+
+func (t *CodeInterpreter) Description() string {
+	return ""
 }
